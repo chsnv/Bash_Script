3857e935077b8d55e150dde7075748534a6bb2ca
chsnv <cosghun.hasanov@gmail.com>
2022-11-26 00:01:31 +0400

    check backups dir is exist

--- scripts/useradd_backup.sh
+++ scripts/useradd_backup.sh
@@ -1,11 +1,13 @@
-if [!-d "/backups"] {
-      mkdir /backups
-}
+if [ ! -d "/backups/users" ]; then
+      mkdir /backups/users
+else
 paste username password | while read i j
 do if grep -q $i /etc/passwd; then
       getent passwd $i
 else
       useradd $i && echo $j | passwd --stdin $i
-      tar fczP /backups/$i.tar.gz /home/$i #tar fcjP  .*.bz2
+      tar fczP /backups/users/$i.tar.gz /home/$i 
+      #tar fcjP  .*.bz2
       fi
-done       
+done
+fi       
