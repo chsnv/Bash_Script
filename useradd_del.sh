2112675947381457341f94de0ba67b413dd33780
chsnv <cosghun.hasanov@gmail.com>
2022-11-25 09:06:10 +0400

    Useradd and Delele script

---
+++ useradd_del.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for i in $1; do
+    if grep -q $i /etc/passwd; then
+    read -p "$i user already in your system, Delete y/n: " n
+    case $n in 
+    y)
+      userdel -r $i;
+      echo "$i user deleted.";;
+    n)
+      echo "$i user not-deleted.";;
+    *)
+      echo "You've to choose y/n option.";;
+    esac  
+    
+    else
+        useradd $i;
+        echo "$i user added to your sistem."
+     fi            
+done
