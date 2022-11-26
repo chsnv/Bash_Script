2b5587aa70643da13d1440d14c6cadd443674b0b
chsnv <cosghun.hasanov@gmail.com>
2022-11-26 13:19:04 +0400

    check free disk space and mail

---
+++ scripts/check_disk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+THRESHOLD=10
+if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+#mail -s 'Disk Space Alert' admin@example.com << EOF
+echo "$HOSTNAME / partition free space is critically low. Used: $CURRENT%"
+#EOF
+fi
