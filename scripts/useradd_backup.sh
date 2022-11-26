4ba31e3739b5fb76a703bb2480adf1e664c0fab5
chsnv <cosghun.hasanov@gmail.com>
2022-11-26 13:24:09 +0400

    last edit auto set from username and password file

--- scripts/useradd_backup.sh
+++ scripts/useradd_backup.sh
@@ -1,13 +1,16 @@
-if [ ! -d "/backups/users" ]; then
-      mkdir /backups/users
-else
+read -p "Do you want to backup? y/n: " ans
+#set -x
 paste username password | while read i j
 do if grep -q $i /etc/passwd; then
       getent passwd $i
 else
       useradd $i && echo $j | passwd --stdin $i
-      tar fczP /backups/users/$i.tar.gz /home/$i 
-      #tar fcjP  .*.bz2
+      if [[ $ans -eq "y" ]] && [ ! -d "/backups/users" ]; then
+      	mkdir -p /backups/users
+	tar fczP /backups/users/$i.tar.gz /home/$i
+      			#tar fcjP  .*.bz2
+      else
+       getent passwd $i    	      
       fi
-done
-fi       
+    fi
+done 
