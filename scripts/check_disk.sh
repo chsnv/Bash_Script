50fbb74783335e33286e87febf4655cddd79dc65
chsnv <cosghun.hasanov@gmail.com>
2022-11-26 13:19:28 +0400

    check disk space over 80% and mail

--- scripts/check_disk.sh
+++ scripts/check_disk.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
-THRESHOLD=10
+THRESHOLD=80
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-#mail -s 'Disk Space Alert' admin@example.com << EOF
+mail -s 'Disk Space Alert' admin@example.com << EOF
 echo "$HOSTNAME / partition free space is critically low. Used: $CURRENT%"
-#EOF
+EOF
 fi
